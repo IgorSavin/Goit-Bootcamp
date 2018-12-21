5b2c5f1dea58504f8c9ca720373c9dc03b0a5393
Igor Savin <beixiaofeng@gmail.com>
2018-12-21 15:40:28 +0100

    some exc

--- vue-firebase/app.js
+++ vue-firebase/app.js
@@ -2,11 +2,35 @@ new Vue({
   el: '#app',
   data:{
     title:'Becoming a Vue ninja',
-    name: 'Ryu'
+    name: 'Igor'
+    // url:'http://www.youtube.com',
+    // classes:['one', 'two']
+    // wage: 10
+    // coords:{
+    //   x:0,
+    //   y:0
+    // }
   },
   methods:{
-    greet(time){
-      return `Hello and good ${time}, ${this.name}`
+    // greet(time){
+    //   return `Hello and good ${time}, ${this.name}`
+    // }
+    // changeWage(amount){
+    //   this.wage+=amount
+    // }
+    // logEvent(e){
+    //   console.log(e);
+    // },
+    // logCoords(e){
+    //   this.coords.x = e.offsetX
+    //   this.coords.y = e.offsetY
+    // }
+    // updateName(e){
+    //   // console.log(e.target.value)  
+    //   this.name = e.target.value
+    // }
+    logMessage(e){
+      console.log('hello world');
     }
   }
 })
