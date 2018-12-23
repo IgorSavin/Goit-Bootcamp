dce791d432075f3d811e4ee877d594201c55a258
Igor Savin <beixiaofeng@gmail.com>
2018-12-23 14:17:25 +0100

    temp commit

---
+++ vue-firebase/module-01/myproject/src/AllFriends.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="all-friends">
+    <h2>All Friends</h2>
+    <div v-for="(friend, index) in buddies" :key="index">
+      <span>{{ friend.name }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AllFriends',
+  props:['buddies'],
+  data(){
+    return{
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
